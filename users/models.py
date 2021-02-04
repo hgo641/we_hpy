59063e5051a8130a617d453d83304568fa9dc906
hgo641 <65850682+hgo641@users.noreply.github.com>
2021-02-04 19:21:53 +0900

    model_user_base

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+        OTHER = ("other", "Other")
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50) 
+    email = models.EmailField(max_length=254)
+    gender = models.CharField(max_length=1,choices = GenderChoices)
+    birth = models.CharField(max_length=50)
+    
