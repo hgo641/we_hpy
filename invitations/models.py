9345369da86488b06af7cbabb1b0acc6b2d1a27b
kimeunseo58 <alan0221@naver.com>
2021-02-05 22:09:49 +0900

    message_modle_1

--- invitations/models.py
+++ invitations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Invitation(models.Model):
+    invitation_number = models.CharField(max_length=50)
+    userID = models.CharField(max_length=50)
+    studyroon_number = models.CharField(max_length=50)
+    message = models.TextField()
--- messages/models.py
+++ messages/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Message(models.Model):
-    messagenumber = models.IntegerField(default=1)
+    messagenumber = models.CharField(max_length=50)
     userID = models.CharField(max_length=50)
     message = models.TextField()
