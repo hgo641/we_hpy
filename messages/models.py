293a8a3128b5cb0220725bf083ea246dd44e9d28
kimeunseo58 <alan0221@naver.com>
2021-02-05 22:01:35 +0900

    test

--- messages/models.py
+++ messages/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
-#for test
-
-#test again...
+class Message(models.Model):
+    messagenumber = models.IntegerField(default=1)
+    userID = models.CharField(max_length=50)
+    message = models.TextField()
