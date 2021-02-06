ad8bbfd383c7308978ec0083c7fce7b93304a0a4
hgo641 <65850682+hgo641@users.noreply.github.com>
2021-02-06 16:42:20 +0900

    eunseo58

--- invitations/models.py
+++ invitations/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Invitation(models.Model):
     invitation_number = models.AutoField(primary_key=True)
     userID = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    studyroom_number = models.ForeignKey(studyrooms.Studyroom_status, on_delete=models.CASCADE)
+    studyroom_number = models.ForeignKey("studyrooms.Studyroom", on_delete=models.CASCADE)
     message = models.TextField()
 
     
