9582411f6265e1deba9d4e9f7a99861705a6597b
hgo641 <65850682+hgo641@users.noreply.github.com>
2021-02-06 16:20:06 +0900

    eunseo58과 합치고 merge

--- invitations/models.py
+++ invitations/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Invitation(models.Model):
     invitation_number = models.AutoField(primary_key=True)
-    userID = models.ForeignKey(users.User, on_delete=models.CASCADE)
+    userID = models.ForeignKey("users.User", on_delete=models.CASCADE)
     studyroom_number = models.ForeignKey(studyrooms.Studyroom_status, on_delete=models.CASCADE)
     message = models.TextField()
 
--- messages2/models.py
+++ messages2/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
     messagenumber = models.AutoField(primary_key=True)
-    userID = models.ForeignKey(users.User, on_delete=models.CASCADE)
+    userID = models.ForeignKey("users.User", on_delete=models.CASCADE)
     message = models.TextField()
     
