f9ff45c6cb581a82639e46f20fe5ec90fc68ea87
hgo641 <65850682+hgo641@users.noreply.github.com>
2021-02-17 21:21:15 +0900

    IntegrityError user.gender

--- users/models.py
+++ users/models.py
@@ -1,24 +1,45 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import User, AbstractUser, BaseUserManager
+from django.conf import settings
 # Create your models here.
 
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, gender, birth_date, password):
+        if not email:
+            raise ValueError("Email Needed")
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            gender=gender,
+            birth_date=birth_date,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractUser):
-    #username email birth_date gender
+    # username email birth_date gender
+    #username = models.CharField(max_length=50)
     class GenderChoices(models.TextChoices):
         MALE = ("m", "Male")
         FEMALE = ("f", "Female")
         OTHER = ("o", "Other")
-    #username = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
-    gender = models.CharField(max_length=1,choices = GenderChoices.choices, unique = True)
-    birth_date = models.CharField(max_length=50)
+    gender = models.CharField(
+        max_length=1, choices=GenderChoices.choices, unique=True)
+    birth_date = models.DateField()
+    objects = UserManager()
 
 
 class My_page(models.Model):
-    #nickname  my_calender 
-    userId = models.ForeignKey("User", on_delete=models.CASCADE, null=False, blank=False)
-    profile_picture = models.ImageField(blank = True, upload_to="user/image/%Y/%m/%d", height_field=None, width_field=None, max_length=None)
-    make_study = models.ManyToManyField("studyrooms.Studyroom", related_name = "make_study") 
-    study_room = models.ManyToManyField("studyrooms.Studyroom", related_name = "study_room")
-
+    # nickname  my_calender
+    userId = models.ForeignKey(
+        "User", on_delete=models.CASCADE, null=False, blank=False)
+    profile_picture = models.ImageField(
+        blank=True, upload_to="user/image/%Y/%m/%d", height_field=None, width_field=None, max_length=None)
+    make_study = models.ManyToManyField(
+        "studyrooms.Studyroom", related_name="make_study")
+    study_room = models.ManyToManyField(
+        "studyrooms.Studyroom", related_name="study_room")
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
 
+
+def sign_up(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        gender = request.POST["gender"]
+        birth_date = request.POST["birth_date"]
+
+        user = User.objects.create_user(
+            username, email, gender, birth_date, password)
+        #user.username = username
+        #user.password = password
+        # user.gender = gender
+        # user.birth_date = birth_date
+        user.save()
+
+        return render(request, "login_test.html")
+    return render(request, "sign_up.html")
 # Create your views here.
