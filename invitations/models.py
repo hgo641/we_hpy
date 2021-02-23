a2145095c852a82c4c2cb226aac822918b1295f3
ChungRan <riplac0314@gmail.com>
2021-02-23 14:49:07 +0900

    Revert "Revert "Revert "Revert "Merge pull request #10 from hgo641/eunseo58""""
    This reverts commit 846c7eb68301691c55f9c72175ab6cecc6500660.

--- invitations/models.py
+++ invitations/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Invitation(models.Model):
-    invitation_number = models.AutoField(primary_key=True)
-    userID = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    studyroom_number = models.ForeignKey("studyrooms.Studyroom", on_delete=models.CASCADE)
-    message = models.TextField()
+class Conversation(models.Model):
 
-    
+    ''' 단톡방 '''
+    members = models.ManyToManyField("users.User", on_delete=models.CASCADE)
+    studyroom = models.OneToOneField("studyrooms.Studyroom", on_delete=models.CASCADE)
+    # studyroom.leader
+    title = models.CharField(max_length= 50)
+
+class Message(models.Model):
+    conversation = models.ForeignKey("Conversation", on_delete = models.CASCADE, related_name= "messages")
+    author = models.ManyToManyField("users.User", related_name="messages")
+    # 유효성 검사 : members 안에 속해있어야 한다.
+    text = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
