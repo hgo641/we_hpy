5b7a5b6463d69bf3d261e24dc15a8dba291e568e
ChungRan <riplac0314@gmail.com>
2021-02-27 20:03:23 +0900

    work myStudyroom page
    .container__footer 추후에 수정해야 함.

--- studyrooms/views.py
+++ studyrooms/views.py
@@ -5,12 +5,25 @@ from django.contrib import auth
         
 def studyroomMake(request):
     if request.user.is_authenticated:
-        return render(request, 'make.html')
+        context = {
+            'tags' : [
+                {'name' : 'A'},
+                {'name' : 'B'},
+                {'name' : 'C'},
+            ]
+        }
+        return render(request, 'make.html', context)
     else:
         return redirect('login')
 
 def studyroomMy(request):
     if request.user.is_authenticated:
-        return render(request, 'my.html')
+        context = {
+            'studyrooms' : [
+                {'subject': 'subjectTest', 'content' : 'contentText'},
+                {'subject': '제목 테스트2', 'content' : '내용 테스트2'},
+            ]
+        }
+        return render(request, 'my.html', context)
     else:
         return redirect('login')
