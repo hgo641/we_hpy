6c0b5f5869cfbe941b6e42e66486ae0160e638a8
hgo641 <65850682+hgo641@users.noreply.github.com>
2021-03-03 22:57:59 +0900

    .

--- invitations/models.py
+++ invitations/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Conversation(models.Model):
 
     ''' 단톡방 '''
-    members = models.ManyToManyField("users.User", on_delete=models.CASCADE)
-    studyroom = models.OneToOneField("studyrooms.Studyroom", on_delete=models.CASCADE)
+    members = models.ManyToManyField("users.User")
+    studyroom = models.OneToOneField(
+        "studyrooms.Studyroom", on_delete=models.CASCADE)
     # studyroom.leader
-    title = models.CharField(max_length= 50)
+    title = models.CharField(max_length=50)
+
 
 class Message(models.Model):
-    conversation = models.ForeignKey("Conversation", on_delete = models.CASCADE, related_name= "messages")
+    conversation = models.ForeignKey(
+        "Conversation", related_name="messages", on_delete=models.CASCADE)
     author = models.ManyToManyField("users.User", related_name="messages")
     # 유효성 검사 : members 안에 속해있어야 한다.
-    text = models.CharField(max_length=500)
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- studyrooms/views.py
+++ studyrooms/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.contrib import auth
 
-        
+
 def studyroomMake(request):
     return render(request, 'make.html')
+
+
+def join(request):
+    return render(request, 'join.html')
+
+
+def myRoom(request):
+    return render(request, 'studyroom.html')
