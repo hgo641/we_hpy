c627b29a0d3c19b8f64cc1bc61c7615ce7812d15
Ink-One123 <78547129+Ink-One123@users.noreply.github.com>
2021-03-11 13:23:10 +0900

    .

--- studyrooms/urls.py
+++ studyrooms/urls.py
@@ -1,6 +1,5 @@
 
 
-
 from django.urls import path, include
 from . import views
 from django.contrib import admin
@@ -8,16 +7,15 @@ from django.conf import settings
 
 
 urlpatterns = [
+    path('make/', views.studyroomMake, name='studyroomMake'),
+    path('my/', views.studyroomMy, name='studyroomMy'),
+    path('join/', views.studyroomJoin, name='studyroomJoin'),
+
     # path('/studyroom', views.login, name='studyroom'),
     # path('/studyroom/found', views.login, name='studyroom'),
-    path('make/', views.studyroomMake, name='studyroomMake'),
-
-    # path('/studyroom/my', views.login, name='studyroom'),
-
-    # path('/studyroom/room/<roomNum>', views.login, name='studyroom'),
-    # path('/studyroom/room/<roomNum>/private', views.login, name= 'studyroom'),
-    # path('/studyroom/room/<roomNum>/public', views.login, name='studyroom'),
-
-]
-
     
+    path('room/<int:room_id>', views.studyroom, name='studyroom'),
+    path('room/<int:room_id>/private', views.studyroomPrivate, name= 'studyroomPrivate'),
+    path('room/<int:room_id>/public', views.studyroomPublic, name='studyroomPublic'),
+    
+]
