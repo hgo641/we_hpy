798028e5300d08e358282975fcbae6eb33d70e8f
hgo641 <65850682+hgo641@users.noreply.github.com>
2021-04-03 23:41:49 +0900

    add board thema

--- boards/models.py
+++ boards/models.py
@@ -8,10 +8,18 @@ class Post(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     modify_date = models.DateTimeField(auto_now=True)
-    thema = models.CharField(max_length=200) #자유게시판/ 질문게시판/ 정보게시판 이런거...
+    THEMA_CHOICES = (
+        ('n','공지게시판'),
+        ('f','자유게시판'),
+        ('q','질문게시판'),
+        ('i','정보게시판'),
+    )
+    thema = models.CharField(max_length=1,choices = THEMA_CHOICES) #자유게시판/ 질문게시판/ 정보게시판 이런거...
     author = models.OneToOneField(
         "users.User", on_delete=models.CASCADE)
     studyroom = models.ForeignKey('studyrooms.Studyroom', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
