756771c1b808cbc9dfaa40c220c2533785976867
hgo641 <65850682+hgo641@users.noreply.github.com>
2021-04-05 21:12:58 +0900

    delete alert

--- boards/urls.py
+++ boards/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('board/<str:board_id>/<int:studyroom_id>', views.board, name='board'),
     path('new/<int:room_id>/<str:board_thema>', views.postnew, name='postnew'),
     path('study/<str:board_thema>/<int:room_id>', views.postcreate, name='postcreate'),
-    
+    path('detail/<int:post_id>',views.detail,name="detail"),
+    path('postdelete/<int:post_id>',views.postdelete,name="postdelete"),
 ]
