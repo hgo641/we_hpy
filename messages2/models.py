db19dc5f589df3ac4e2661d943051f8ee8a10ce0
ChungRan <riplac0314@gmail.com>
2021-04-12 03:52:49 +0900

    clean up unused code

--- messages2/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Message(models.Model):
-    messagenumber = models.AutoField(primary_key=True)
-    userID = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    message = models.TextField()
-    
