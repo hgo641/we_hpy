02b8d4b2e726a7857d3a0486aaf87f02e71115ea
ChungRan <riplac0314@gmail.com>
2021-04-12 14:58:53 +0900

    studyroom - user 중간 모델 수정

--- users/models.py
+++ users/models.py
@@ -20,22 +20,22 @@ class User(AbstractUser):
     profile_picture = models.ImageField(
         blank=True, upload_to="user/image/%Y/%m/%d", height_field=None, width_field=None, max_length=None)
     study_room = models.ManyToManyField(
-        "studyrooms.Studyroom", related_name="users")
+        "studyrooms.Studyroom", related_name="users", through="studyrooms.Progress_rate")
 
 
-@receiver(m2m_changed, sender=User.study_room.through)
-def my_callback(sender, **kwargs):
-    print(kwargs)
-    if kwargs['reverse'] == True:
-        studyroom = kwargs['instance']
-        user = User.objects.get(id=list(kwargs['pk_set'])[0])
-    else:
-        studyroom = Studyroom.objects.get(id=list(kwargs['pk_set'])[0])
-        user = kwargs['instance']
+# @receiver(m2m_changed, sender=User.study_room.through)
+# def my_callback(sender, **kwargs):
+#     print(kwargs)
+#     if kwargs['reverse'] == True:
+#         studyroom = kwargs['instance']
+#         user = User.objects.get(id=list(kwargs['pk_set'])[0])
+#     else:
+#         studyroom = Studyroom.objects.get(id=list(kwargs['pk_set'])[0])
+#         user = kwargs['instance']
 
-    if kwargs['action'] == "pre_add":
-        print("pre_add")
-        Progress_rate.objects.create(user=user, studyroom=studyroom)
-    elif kwargs['action'] == "pre_remove":
-        print("pre_remove")
-        Progress_rate.objects.get(user=user, studyroom=studyroom).delete()
+#     if kwargs['action'] == "pre_add":
+#         print("pre_add")
+#         Progress_rate.objects.create(user=user, studyroom=studyroom)
+#     elif kwargs['action'] == "pre_remove":
+#         print("pre_remove")
+#         Progress_rate.objects.get(user=user, studyroom=studyroom).delete()
